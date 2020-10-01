c8d44b9b4ecc8111d1fc19d5de0ac9d93059dca6
Ishmam <techopoly444@gmail.com>
2020-10-01 21:11:03 +0600

    17-5

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -4,13 +4,30 @@ const output = document.querySelector('p');
 function trackUserHandler() {
   console.log('Clicked!');
 }
-function function1(){
+
+// 17(5)
+function function2(posData) {
+  console.log(posData)  
+}
+
+// setTimeout(() => {
+//   console.log('this is executed after 3 sec')
+// }, 3000);
+
+button.addEventListener('click', trackUserHandler);
+//function1();
+//navigator.geolocation.getCurrentPosition(function2)
+
+
+//working on how the geoloacation uses the callback function and how to pass parameter
+
+function function1(aFuntion){
+  let name = 'ihmma'
+  aFuntion(name);
   console.log('this is executed immediately')
 }
 
-setTimeout(() => {
-  console.log('this is executed after 3 sec')
-}, 3000);
-
-button.addEventListener('click', trackUserHandler);
-function1();
+function f1(name){
+  console.log(name)
+  }
+function1(f1)
