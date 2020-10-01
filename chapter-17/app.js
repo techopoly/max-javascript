6b381b15e72b52ad7f7da6d4ffe7b8c19fc74c8e
Ishmam <techopoly444@gmail.com>
2020-10-01 19:54:28 +0600

    changed chapter 17

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -4,5 +4,13 @@ const output = document.querySelector('p');
 function trackUserHandler() {
   console.log('Clicked!');
 }
+function function1(){
+  console.log('this is executed immediately')
+}
+
+setTimeout(() => {
+  console.log('this is executed after 3 sec')
+}, 3000);
 
 button.addEventListener('click', trackUserHandler);
+function1();
