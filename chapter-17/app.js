826852b4a6461d664729a823894b676c14f50382
Ishmam <techopoly444@gmail.com>
2020-10-02 23:41:18 +0600

    updated

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -1,19 +1,72 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-function trackUserHandler() {
-  console.log('Clicked!');
+// 17(8)
+
+function setTimer(duration) {
+  const promise = new Promise((resolve, reject) => { //  this inside function is executed right after creating the promice object
+    setTimeout(() => {
+      resolve('done!')
+    },
+      duration)
+  })
+
+  return promise
 }
 
-// 17(8)
-function function2(posData) {
-  console.log(posData)
+function trackUserHandler(){
+  navigator.geolocation.getCurrentPosition(posData => {
+      setTimer(2000).then(data => {
+        console.log(data, posData)
+      })
+  },
+  error => {
+    console.log('error') 
+  })
 }
-/* set timeout has no promise version */
-// setTimeout(() => {
-//   console.log('this is executed after 3 sec')
-// }, 3000);
+
+/* lets do the same in trivial way */
+// function trackUserHandler() {
+//   navigator.geolocation.getCurrentPosition((posData) => {
+//     setTimeout(() => {
+//       console.log(posData)
+//     }, 2000)
+//   })
+// }
 
 button.addEventListener('click', trackUserHandler);
 
+
+
+
+
+
+
+console.log('---------------------')
+
+// other insight gained from this section
+// very very very important concept.
+/* i know it but i make mistake when things get complicated. see below explanation next to each line */
+let name = 'ishmam'
+
+function a(c1) {
+ c1();
+}
+
+a((name) => { /* this name is not the 'name' variable declared above.  when we pass the parameter in function 
+definition it has nothing to do with other variables with same name*/
+  console.log(name) 
+})
+
+function b(name){ /* this name is not the 'name' variable declared above.  when we pass the parameter in function 
+definition it has nothing to do with other variables with same name*/
+  console.log(name)
+}
+
+b();
+a(() => {
+  console.log(name); /* this will log 'ishmam' as we are using the name variable decalred above. instead of passing
+any new argument. this callback function is inside the same scope where the name variable is declared and thats why
+we can access that inside the callback function */
+})
 
