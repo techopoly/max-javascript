52ffdb776f53b644863e59bf5c2c586de40e6a6b
Ishmam <techopoly444@gmail.com>
2020-10-03 13:03:57 +0600

    'this'

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -2,6 +2,19 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 // 17(9)
+// now lets promisify the location method
+function getPosition() {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(posData => {  
+      resolve(posData)
+    },
+      error => {
+        console.log('error')
+      })
+  })
+  return promise
+}
+
 
 function setTimer(duration) {
   const promise = new Promise((resolve, reject) => { //  this inside function is executed right after creating the promice object
@@ -10,28 +23,26 @@ function setTimer(duration) {
     },
       duration)
   })
-
   return promise
 }
 
-function trackUserHandler(){
-  navigator.geolocation.getCurrentPosition(posData => {
-      setTimer(2000).then(data => {
-        console.log(data, posData)
-      })
-  },
-  error => {
-    console.log('error') 
+function trackUserHandler() {
+  getPosition().then(pos => {
+    setTimer(2000).then(data => {
+      console.log(data, pos)
+    })
   })
 }
 
-/* lets do the same in trivial way */
 // function trackUserHandler() {
-//   navigator.geolocation.getCurrentPosition((posData) => {
-//     setTimeout(() => {
-//       console.log(posData)
-//     }, 2000)
-//   })
+//   navigator.geolocation.getCurrentPosition(posData => {
+//     setTimer(2000).then(data => {
+//       console.log(data, posData)
+//     })
+//   },
+//     error => {
+//       console.log('error')
+//     })
 // }
 
 button.addEventListener('click', trackUserHandler);
