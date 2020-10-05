d3db1c1ebc054955b2f1cf39128e5c06f703b04f
Ishmam <techopoly444@gmail.com>
2020-10-05 17:04:03 +0600

    17-13

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
+//17-13: async await and error handling
 
 //promisify geolocation method
 
@@ -15,20 +16,27 @@ async function getPosition() {
   })
   return promise
 }
-
-async function set(duration){
+//promisify setTimeout method
+async function set(duration) {
   const promise = new Promise((res, rej) => {
     setTimeout(() => {
       res('done')
     },
-    duration)
+      duration)
   })
   return promise
 }
 
 async function trackUserHandler() {
-  const position = await getPosition()
-  const timerData = await set(2000);
+  let position; // changing them from try block scope to one scope up
+  let timerData;
+  try {
+    position = await getPosition()
+    timerData = await set(2000);
+  } catch (error) {
+    console.log(error.message);
+  }
+
   console.log(timerData, position)
 }
 
