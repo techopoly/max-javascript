c49e8aabe8f6ab309252d47369c744da266f18f9
Ishmam <techopoly444@gmail.com>
2020-10-05 19:50:20 +0600

    17-14

--- chapter-17/app.js
+++ chapter-17/app.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
-//17-13: async await and error handling
+//17-14: async await and error handling
 
 //promisify geolocation method
 
@@ -27,6 +27,10 @@ async function set(duration) {
   return promise
 }
 
+/* one down side of async await is everything inside that async function is chained with then. So test it. lets add
+other async functions inside trackUserHandler and see that those functions are executed only after the upper 
+functions are done with execution */
+
 async function trackUserHandler() {
   let position; // changing them from try block scope to one scope up
   let timerData;
@@ -36,9 +40,11 @@ async function trackUserHandler() {
   } catch (error) {
     console.log(error.message);
   }
+  console.log(timerData, position);
 
-  console.log(timerData, position)
+  console.log('getting position'); /* in the .then() chaining this function doesnt depend on the execution of
+  other function above this one. but in async await the will execute after the above functions are done executing.
+  see the console if you dont believe me. also wathc the lecture if you dont understand the concept */
 }
-
 
 button.addEventListener('click', trackUserHandler)
