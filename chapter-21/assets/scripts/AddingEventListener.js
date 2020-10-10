e092b59530c960d85c1fd6bb0acec4480741154d
Ishmam <techopoly444@gmail.com>
2020-10-10 23:44:57 +0600

    chapter-8:added eslint from npm and installed eslint in IDE

--- chapter-21/assets/scripts/AddingEventListener.js
+++ chapter-21/assets/scripts/AddingEventListener.js
@@ -4,9 +4,9 @@ import { FinishToActive } from './FinishToActive.js'
 export function addingEventListener(type) {
     const handler = type === 'active' ? activeToFinish : FinishToActive
     const allProjects = document.querySelectorAll(`#${type}-projects li`);
-    console.log(allProjects)
+    console.log(allProjects);
     for (const eachProject of allProjects) {
-        const button = eachProject.querySelector('button:last-of-type')
-        button.addEventListener('click', handler.bind(null, eachProject))
+        const button = eachProject.querySelector('button:last-of-type');
+        button.addEventListener('click', handler.bind(null, eachProject));
     }
 }
