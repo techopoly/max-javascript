538c8b332d8fc7a4a0d0d4ef6130c46fc70a2bd4
Ishmam <techopoly444@gmail.com>
2020-10-13 21:40:06 +0600

    23-8

--- chapter-23/src/app.js
+++ chapter-23/src/app.js
@@ -1,12 +1,18 @@
-/* 23-5
-now we will work on an api which is supported on the chrome browser but not on the edge. so we will find a way to 
-work around that. 
+/* 23-8
+we will use both babel for browser support of differnt javascript features and polyfill for using the
+advanced concepts like promises that can be made by older syntax with a diff approach
+but to do that we need a npm library core-js(stores all the polyfills) which is a lot bigger than our entire project. so to solve
+that problem we will use the specific feature that we need from core-js.but now there arises another problem
+which is we still need to manually import all the features. to avoid that we will tell babel to 
+do auto polyfilling for us, in other word automaically import the polyfills that our application needs.
+we change that at webpackl.config file.
 */
-
+//import 'core-js/features/promise'
 const button = document.querySelector('button');
 const paragraph = document.querySelector('p')
 
-
+const  promise = new Promise();// adding this just to test if babel automatically adds polyfills for this promise
+console.log(promise) 
 button.addEventListener('click', () => {
   if (navigator.clipboard) {
     const text = paragraph.textContent
