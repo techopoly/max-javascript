a0a5eebc45f4e69bd361338ed152ac0d390b5b4a
Ishmam <techopoly444@gmail.com>
2020-10-16 17:35:51 +0600

    27-10

--- chapter-27/app.js
+++ chapter-27/app.js
@@ -1,14 +1,25 @@
+/* 
+27-10
+*/
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next)=>{
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
     res.header('Content-Type', 'text/html');
     next()
 });
 
-app.use((req, res, next)=>{
-    res.send('<h1>hello world</h1>')
+app.use((req, res, next) => {
+    const username = req.body.username || 'Unknown User'
+    res.send(`
+    <form method="post" action="/" > 
+        <input type="text" name="username" placeholder="type your name"> 
+        <button type ="submit"> Send </button>
+    </form>
+    <h1>Hi ${username} </h1>`)
 })
 app.listen(3000);
-
