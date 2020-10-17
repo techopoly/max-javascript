85a581b2f86d6973f53aefb18eb2e4e1bf8b8c9c
Ishmam <techopoly444@gmail.com>
2020-10-17 21:46:47 +0600

    31-5

--- chapter-31/app.js
+++ chapter-31/app.js
@@ -1,4 +1,4 @@
-const { generateText, createElement, validateInput } = require('./util');
+const { checkAndGenerate, createElement, validateInput } = require('./util');
 
 const initApp = () => {
   // Initializes the app, registers the button click listener
@@ -11,19 +11,18 @@ const addUser = () => {
   // and appends the element to the DOM
   const newUserNameInput = document.querySelector('input#name');
   const newUserAgeInput = document.querySelector('input#age');
-
-  if (
-    !validateInput(newUserNameInput.value, true, false) ||
-    !validateInput(newUserAgeInput.value, false, true)
-  ) {
-    return;
-  }
-
-  const userList = document.querySelector('.user-list');
-  const outputText = generateText(
+  const outputText = checkAndGenerate(
     newUserNameInput.value,
     newUserAgeInput.value
   );
+
+  if(!outputText){
+     return;
+  }
+  
+
+  const userList = document.querySelector('.user-list');
+  
   const element = createElement('li', outputText, 'user-item');
   userList.appendChild(element);
 };
--- chapter-31/util.test.js
+++ chapter-31/util.test.js
@@ -1,13 +1,29 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', ()=>{
     const text = generateText('Ishmam', 23);
     expect(text).toBe('Ishmam (23 years old)');
     const text2 = generateText('Tasin', 15);
-    expect(text2).toBe('Tasin (15 years old)')
+    expect(text2).toBe('Tasin (15 years old)');
 });
 
 test('should output data-less text', ()=>{
     const text = generateText('', null);
-    expect(text).toBe(' (null years old)')
-})
+    expect(text).toBe(' (null years old)');
+});
+
+//31-5
+/* integration test: 
+we take a funtion that has dependencies which means that function run other function inside that function and if 
+those inside functions fail to te execute then the whole function fails.
+thats why we do unit testing for all those inside function. ex: the 2 test above is unit tested
+and the function below is integration test which tests if all the pieces of all unit is syncing properly when they 
+are executed as dependencies. we are doing integration test for checkAndGenerate()
+so even though units might work properly they can fail when combined together and one depends on the other and thats
+why we do integration testing 
+*/
+
+test('should check and generate a valid text', ()=>{
+    const text = checkAndGenerate('md', 40);
+    expect(text).toBe('md (40 years old)');
+});
